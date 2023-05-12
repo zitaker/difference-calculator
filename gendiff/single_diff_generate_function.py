5461f25766882ef0bce40bd71338710c51faac23
georg <georbearwolf.com>
2023-05-12 10:43:42 +0300

    [017] error flake8

---
+++ gendiff/single_diff_generate_function.py
@@ -0,0 +1,132 @@
+import json
+from gendiff.constants import PLUS, MINUS, SPASE
+
+
+# def diff_generate(file1, file2):  # noqa: C901
+#     # вводим 2 файла
+#     # file1 = open('gendiff/tests/fixtures/file1.json')
+#     # file2 = open('gendiff/tests/fixtures/file2.json')
+#
+#     # определяем два файла для чтения человека
+#     obj1 = json.loads(file1.read())
+#     obj2 = json.loads(file2.read())
+#
+#     # переводим файлы в списки
+#     obj1_translate_in_list = list(obj1.items())
+#     obj2_translate_in_list = list(obj2.items())
+#
+#     # создаем единый список
+#     unified_obj_list = list()
+#
+#     for key in obj1_translate_in_list:
+#         if key in obj2_translate_in_list:
+#             unified_obj_list.append(f"{SPASE}{key}")
+#
+#     for key in obj1_translate_in_list:
+#         if key not in obj2_translate_in_list:
+#             unified_obj_list.append(f"{MINUS}{key}")
+#
+#     for key in obj2_translate_in_list:
+#         if key not in obj1_translate_in_list:
+#             unified_obj_list.append(f"{PLUS}{key}")
+#
+#     # начинаем редактировать единый список
+#     unified_obj_list1 = str(unified_obj_list)
+#     unified_obj_list2 = unified_obj_list1.split('",')
+#
+#     # исключаем дубликаты
+#     list_without_duplicates = []
+#     for key in unified_obj_list2:
+#         if key not in list_without_duplicates:
+#             list_without_duplicates.append(key)
+#     # print(list_without_duplicates)
+#
+#
+#     # исключаем лишние символы
+#     str_without_duplicates = str(list_without_duplicates)
+#     str_without_duplicates1 = str_without_duplicates.replace("[", '')
+#     str_without_duplicates2 = str_without_duplicates1.replace("]", '')
+#     str_without_duplicates3 = str_without_duplicates2.replace("\\',", ':')
+#     str_without_duplicates4 = str_without_duplicates3.replace("'", '')
+#     str_without_duplicates5 = str_without_duplicates4.replace('"', '')
+#     str_without_duplicates6 = str_without_duplicates5.replace('(', '')
+#     str_without_duplicates7 = str_without_duplicates6.replace(')', '')
+#     str_without_duplicates8 = str_without_duplicates7.replace('\\', '')
+#
+#     # сортируем по алфавиту
+#     list_in_alphabe_order = str_without_duplicates8.split(',  ')
+#     sort_alphabetically = sorted(list_in_alphabe_order, key=lambda x: x[2:])
+#
+#     # исключаем лишние символы
+#     sort_alphabetically_str = str(sort_alphabetically)
+#     sort_alphabetically_str1 = sort_alphabetically_str.replace("[", '')
+#     sort_alphabetically_str2 = sort_alphabetically_str1.replace("]", '')
+#     sort_alphabetically_str3 = sort_alphabetically_str2.replace("'", '')
+#     sort_alphabetically_str4 = sort_alphabetically_str3.replace(",", '\n')
+#     sort_alphabetically_str5 = sort_alphabetically_str4.replace("  ", ' ')
+#     sort_alphabetically_str6 = sort_alphabetically_str5.replace("  ", '   ')
+#
+#     sort_alphabetically_str7 = (f"{'{'}{sort_alphabetically_str6}{'}'}")
+#
+#     sort_alphabetically_str8 = sort_alphabetically_str7.replace("{", '{\n')
+#     sort_alphabetically_str9 = sort_alphabetically_str8.replace("}", '\n}')
+#
+#     sort_alphabetically_str10 = sort_alphabetically_str9.lower()
+#     return sort_alphabetically_str10
+#
+#
+# # вводим 2 файла
+# print(diff_generate(open('tests/fixtures/file1.json'),
+#                     open('tests/fixtures/file2.json')))
+
+
+#                                           сокращение функций за счет тестов
+# def exclusion_duplicates():
+#     unified_obj_list = translate_to_single_list()
+#
+#     # редактируем список
+#     unified_obj_list1 = str(unified_obj_list)
+#     unified_obj_list2 = unified_obj_list1.split('",')
+#
+#     # исключаем дубликаты
+#     list_without_duplicates = []
+#     for key in unified_obj_list2:
+#         if key not in list_without_duplicates:
+#             list_without_duplicates.append(key)
+#
+#     return list_without_duplicates
+#
+#
+# def diff_generate(file1, file2):
+#     list_without_duplicates = exclusion_duplicates()
+#     # исключаем лишние символы
+#     str_without_duplicates = str(list_without_duplicates)
+#     str_without_duplicates1 = str_without_duplicates.replace("[", '')
+#     str_without_duplicates2 = str_without_duplicates1.replace("]", '')
+#     str_without_duplicates3 = str_without_duplicates2.replace("\\',", ':')
+#     str_without_duplicates4 = str_without_duplicates3.replace("'", '')
+#     str_without_duplicates5 = str_without_duplicates4.replace('"', '')
+#     str_without_duplicates6 = str_without_duplicates5.replace('(', '')
+#     str_without_duplicates7 = str_without_duplicates6.replace(')', '')
+#     str_without_duplicates8 = str_without_duplicates7.replace('\\', '')
+#
+#     # сортируем по алфавиту
+#     list_in_alphabe_order = str_without_duplicates8.split(',  ')
+#     sort_alphabetically = sorted(list_in_alphabe_order, key=lambda x: x[2:])
+#
+#     # исключаем лишние символы
+#     sort_alphabetically_str = str(sort_alphabetically)
+#     sort_alphabetically_str1 = sort_alphabetically_str.replace("[", '')
+#     sort_alphabetically_str2 = sort_alphabetically_str1.replace("]", '')
+#     sort_alphabetically_str3 = sort_alphabetically_str2.replace("'", '')
+#     sort_alphabetically_str4 = sort_alphabetically_str3.replace(",", '\n')
+#     sort_alphabetically_str5 = sort_alphabetically_str4.replace("  ", ' ')
+#     sort_alphabetically_str6 = sort_alphabetically_str5.replace("  ", '   ')
+#
+#     sort_alphabetically_str7 = (f"{'{'}{sort_alphabetically_str6}{'}'}")
+#
+#     sort_alphabetically_str8 = sort_alphabetically_str7.replace("{", '{\n')
+#     sort_alphabetically_str9 = sort_alphabetically_str8.replace("}", '\n}')
+#
+#     sort_alphabetically_str10 = sort_alphabetically_str9.lower()
+#     return sort_alphabetically_str10
--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -4,35 +4,42 @@ from gendiff.generate_diff import translate_to_single_list
 from gendiff.generate_diff import diff_generate
 
 
-
 def test_read_json_file():
     path = 'gendiff/tests/fixtures/file1.json'
-    expected_result = {'follow': False, 'host': 'hexlet.io', 'proxy': '123.234.53.22', 'timeout': 50}
+    expected_result = {'follow': False, 'host': 'hexlet.io',
+                       'proxy': '123.234.53.22', 'timeout': 50}
+
     assert read_json_file(path) == expected_result
 
 
 def test_translate_to_lists():
-    obj1 = [('host', 'hexlet.io'), ('timeout', 50), ('proxy', '123.234.53.22'), ('follow', False)]
+    obj1 = [('host', 'hexlet.io'), ('timeout', 50), ('proxy', '123.234.53.22'),
+            ('follow', False)]
+
     obj2 = [('timeout', 20), ('verbose', True), ('host', 'hexlet.io')]
-    expected_result1 = [('host', 'hexlet.io'), ('timeout', 50), ('proxy', '123.234.53.22'), ('follow', False)]
-    expected_result2 = [('timeout', 20), ('verbose', True), ('host', 'hexlet.io')]
-    assert translate_to_lists(obj1, obj2) == (expected_result1, expected_result2)
+    expected_result1 = [('host', 'hexlet.io'), ('timeout', 50),
+                        ('proxy', '123.234.53.22'), ('follow', False)]
+
+    expected_result2 = [('timeout', 20), ('verbose', True),
+                        ('host', 'hexlet.io')]
+
+    assert translate_to_lists(obj1, obj2) == \
+           (expected_result1, expected_result2)
 
 
 def test_translate_to_single_list():
     obj1 = None
     obj2 = None
-    expected_result = ["   ('host', 'hexlet.io')", " - ('timeout', 50)", " - ('proxy', '123.234.53.22')",
-                       " - ('follow', False)", " + ('timeout', 20)", " + ('verbose', True)"]
+    expected_result = ["   ('host', 'hexlet.io')", " - ('timeout', 50)",
+                       " - ('proxy', '123.234.53.22')",
+                       " - ('follow', False)", " + ('timeout', 20)",
+                       " + ('verbose', True)"]
     assert translate_to_single_list(obj1, obj2) == expected_result
 
 
-# def test_diff_generate():
-#     result = {
-#                - follow: false
-#                  host: hexlet.io
-#                - proxy: 123.234.53.22
-#                + timeout: 20
-#                - timeout: 50
-#                + verbose: true
-#              }
+def test_diff_generate():
+    path1 = 'gendiff/tests/fixtures/file1.json'
+    path2 = 'gendiff/tests/fixtures/file2.json'
+    test_file = open('gendiff/tests/fixtures/test_result.txt')
+    expected_result = test_file.read()
+    assert diff_generate(path1, path2) == expected_result
