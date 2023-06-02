4a427c73e3f11e297923bd3d3364e213532a0e64
georg_dryndin <georbearwolf.com>
2023-06-02 23:20:43 +0300

    [040] attempt 100501

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -5,35 +5,133 @@ from gendiff.parser import file_parser
 def create_diff_dict(dict_1, dict_2):
     diff_dict = dict()
 
+    # если такая конструкция то сортирует по алфавиту, но пропадают значения
     for key, value in dict_1.items():
-        if isinstance(value, str):
-            if (key, value) in dict_2.items():
-                diff_dict[f"{' = '}{key}"] = value
-
-    for key, value in dict_1.items():
-        if isinstance(value, str):
-            if key not in dict_2.keys():
-                diff_dict[f"{MINUS}{key}"] = value
+        diff_dict[key] = value
 
     for key, value in dict_2.items():
-        if isinstance(value, str):
-            if (key, value) not in dict_1.items():
-                diff_dict[f"{PLUS}{key}"] = value
+        diff_dict[key] = value
 
-    for key, value in dict_1.items():
-        if isinstance(value, dict):
-            if key not in dict_2.keys():
-                diff_dict[f"{MINUS}{key}"] = value
+        """
+        {
+     common: {
+       follow: False
+       setting1: Value 1
+       setting3: None
+       setting4: blah blah
+       setting5: {
+         key5: value5
+        }
+       setting6: {
+         key: value
+         ops: vops
+         doge: {
+           wow: so much
+          }
+        }
+      }
+     group1: {
+       foo: bar
+       baz: bars
+       nest: str
+      }
+     group2: {
+       abc: 12345
+       deep: {
+         id: 45
+        }
+      }
+     group3: {
+       deep: {
+         id: {
+           number: 45
+          }
+        }
+       fee: 100500
+      }
+    }
+    
+        """
 
-    for key, value in dict_2.items():
-        if isinstance(value, dict):
-            if key not in dict_1.keys():
-                diff_dict[f"{PLUS}{key}"] = value
+    # пропадают значения, не по алфавиту, и после знака (~) - думает что
+    # элементы все равны, даже если они отсутствуют в другом словаре, вывод:
+    """
+    {
+        common: {
+          + follow: false
+            setting1: Value 1
+          - setting2: 200
+          - setting3: true
+          + setting3: null
+          + setting4: blah blah
+          + setting5: {
+                key5: value5
+            }
+            setting6: {
+                doge: {
+                  - wow:
+                  + wow: so much
+                }
+                key: value
+              + ops: vops
+            }
+        }
+        group1: {
+          - baz: bas
+          + baz: bars
+            foo: bar
+          - nest: {
+                key: value
+            }
+          + nest: str
+        }
+      - group2: {
+            abc: 12345
+            deep: {
+                id: 45
+            }
+        }
+      + group3: {
+            deep: {
+                id: {
+                    number: 45
+                }
+            }
+            fee: 100500
+        }
+    }
+    """
 
-    for key, value in dict_1.items():
-        if isinstance(value, dict):
-            if key in dict_2.keys():
-                diff_dict[f"{' ~ '}{key}"] = create_diff_dict(value, value)
+
+    # for key, value in dict_1.items():
+    #     if isinstance(value, str):
+    #         if (key, value) in dict_2.items():
+    #             diff_dict[f"{' = '}{key}"] = value
+    #
+    # for key, value in dict_1.items():
+    #     if isinstance(value, str):
+    #         if key not in dict_2.keys():
+    #             diff_dict[f"{MINUS}{key}"] = value
+    #
+    # for key, value in dict_2.items():
+    #     if isinstance(value, str):
+    #         if (key, value) not in dict_1.items():
+    #             diff_dict[f"{PLUS}{key}"] = value
+    #
+    # for key, value in dict_1.items():
+    #     if isinstance(value, dict):
+    #         if key not in dict_2.keys():
+    #             diff_dict[f"{MINUS}{key}"] = value
+    #
+    # for key, value in dict_2.items():
+    #     if isinstance(value, dict):
+    #         if key not in dict_1.keys():
+    #             diff_dict[f"{PLUS}{key}"] = value
+    #
+    # for key, value in dict_1.items():
+    #     if isinstance(value, dict):
+    #         if key in dict_2.keys():
+    #             diff_dict[f"{' ~ '}{key}"] = create_diff_dict(value, value)
 
     return diff_dict
 
