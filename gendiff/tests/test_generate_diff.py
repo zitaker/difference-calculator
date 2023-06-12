82d28a275ef016a72341861f933fdd63c861757c
georg_dryndin <georbearwolf.com>
2023-06-12 22:10:30 +0300

    [048] make tasts and aksinema

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -4,6 +4,10 @@ from gendiff.generate_diff import generate_diff
 def test_generate_diff():
     path1 = 'gendiff/tests/fixtures/file1.json'
     path2 = 'gendiff/tests/fixtures/file2.json'
-    expected_result_plain = open('gendiff/tests/fixtures/test_result_plain.txt').read()
-    expected_result_stylish = open('gendiff/tests/fixtures/test_result_stylish.txt').read()
-    assert generate_diff(path1, path2) == expected_result_plain or expected_result_stylish
+
+    obj_txt_1 = 'gendiff/tests/fixtures/test_result_plain.txt'
+    obj_txt_2 = 'gendiff/tests/fixtures/test_result_stylish.txt'
+
+    expected_plain = open(obj_txt_1).read()
+    expected_stylish = open(obj_txt_2).read()
+    assert generate_diff(path1, path2) == expected_plain or expected_stylish
