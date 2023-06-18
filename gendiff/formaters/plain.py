2f28735bd55f2730ec763b69d65a4ffa8d8300f6
georg_dryndin <georbearwolf.com>
2023-06-18 21:45:40 +0300

    [056] figure out how all the KEY will be displayed, remove the single UNCHANGED ones

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -1,4 +1,4 @@
-def str_template(PROPERTY, key, symbol):
+def plain_template(PROPERTY, key, symbol):
     return f'{PROPERTY} {key} {symbol}'
 
 
@@ -13,7 +13,8 @@ PROPERTY = 'Property'
 symbols_dict = {
                 ADDED: f"{'was added with value'}: {'[complex value]'}",
                 REMOVED: f"{'was removed'}",
-                CHANGED: f"{'was updated. From'}"
+                CHANGED: f"{'was updated. From'}",
+                UNCHANGED: ""
                 }
 
 
@@ -22,50 +23,56 @@ def plain(obj_dict):
     for k, v in obj_dict.items():
         types = v.get('type')
         value = v.get('value')
+        children = v.get('children')
 
-        if types == ADDED or types == REMOVED:
+        if types == UNCHANGED or types == ADDED or types == REMOVED:
             result.append(
-                str_template(PROPERTY, k, symbols_dict[types]))
+                plain_template(PROPERTY, k, symbols_dict[types]))
 
         elif types == CHANGED:
             result.append(
-                str_template(
+                plain_template(
                     PROPERTY, k, f"{symbols_dict[CHANGED]} "
                                  f"'{value.get('old_value')}' "
                                  f"{'to'} '{value.get('new_value')}'"))
+
+        else:
+            result.append(
+                plain_template(PROPERTY, k, f"{symbols_dict[UNCHANGED]} {plain(children)}")
+            )
 
     result = '\n'.join(result)
     return result
 
 
-obj_dict = {
-    'follow': {
-        'type': 'removed',
-        'value': False,
-        'children': None
-    },
-    'host': {
-        'type': 'unchanged',
-        'value': 'hexlet.io',
-        'children': None
-    },
-    'proxy': {
-        'type': 'removed',
-        'value': '123.234.53.22',
-        'children': None
-    },
-    'timeout': {
-        'type': 'changed',
-        'value': {
-            'old_value': 50,
-            'new_value': 20
-        },
-        'children': None
-    },
-    'verbose': {
-        'type': 'added',
-        'value': True,
-        'children': None}
-}
+# obj_dict = {
+#     'follow': {
+#         'type': 'removed',
+#         'value': False,
+#         'children': None
+#     },
+#     'host': {
+#         'type': 'unchanged',
+#         'value': 'hexlet.io',
+#         'children': None
+#     },
+#     'proxy': {
+#         'type': 'removed',
+#         'value': '123.234.53.22',
+#         'children': None
+#     },
+#     'timeout': {
+#         'type': 'changed',
+#         'value': {
+#             'old_value': 50,
+#             'new_value': 20
+#         },
+#         'children': None
+#     },
+#     'verbose': {
+#         'type': 'added',
+#         'value': True,
+#         'children': None}
+# }
 
 # print(plain(obj_dict))
