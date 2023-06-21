ec8e572329bdf1a03a3b0080ffe598957c7feef6
MyWork <92075508+zitaker@users.noreply.github.com>
2023-06-21 22:13:11 +0300

    Add files via upload

--- gendiff/tests/test_generate_diff.py
+++ gendiff/tests/test_generate_diff.py
@@ -9,9 +9,11 @@ def test_generate_diff():
                                'test_result_one_level_formatter.txt'
     path_stylish = 'gendiff/tests/fixtures/test_result_stylish.txt'
     path_plain = 'gendiff/tests/fixtures/test_result_plain.txt'
+    path_json = 'gendiff/tests/fixtures/test_result_json.txt'
 
     expected_result_0 = open(path_one_level_formatter).read()
     expected_result_1 = open(path_stylish).read()
     expected_result_2 = open(path_plain).read()
+    expected_result_3 = open(path_json).read()
     assert generate_diff(path1, path2) == expected_result_0 or \
-           expected_result_1 or expected_result_2
+           expected_result_1 or expected_result_2 or expected_result_3
