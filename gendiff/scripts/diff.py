3b3e84a2c55fa58137a5fb607eadd0f1d02bc922
georg <georbearwolf.com>
2023-06-22 10:46:37 +0300

    fix the error_1

--- gendiff/scripts/diff.py
+++ gendiff/scripts/diff.py
@@ -6,7 +6,8 @@ from gendiff.generate_diff import generate_diff
 def main():
     args = parse_args()
     diff = generate_diff(args.first_file, args.second_file, args.format)
-    return diff
+    # return diff
+    print(diff)
 
 
 if __name__ == '__main__':
