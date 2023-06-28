ae502fadf6686b485426ece67637218b4d922fb2
georg <georbearwolf.com>
2023-06-28 16:25:00 +0300

    [072] we remove the examples from the test modules into files

--- gendiff/tests/test_parser.py
+++ gendiff/tests/test_parser.py
@@ -1,13 +1,10 @@
 from gendiff.generate_diff import file_parser
+import json
 
 
 def test_file_parser():
-    path = 'gendiff/tests/fixtures/file1.json'
-    expected_result = {
-        'follow': False,
-        'host': 'hexlet.io',
-        'proxy': '123.234.53.22',
-        'timeout': 50,
-    }
-    if path.endswith('.json'):
-        assert file_parser(path) == expected_result
+    path_1 = 'gendiff/tests/fixtures/file1.json'
+    path_2 = open('gendiff/tests/fixtures/test_file_parser.json')
+    expected_result = json.loads(path_2.read())
+    if path_1.endswith('.json'):
+        assert file_parser(path_1) == expected_result
--- gendiff/tests/test_plain.py
+++ gendiff/tests/test_plain.py
@@ -4,152 +4,22 @@ from gendiff.formaters.plain import make_str
 
 
 def test_make_str():
-    obj_dict = {
-        'follow': {
-            'type': 'removed',
-            'value': False,
-            'children': None
-        }
-    }
+    path_1 = open('gendiff/tests/fixtures/test_make_str_if.json')
+    expected_result = json.loads(path_1.read())
 
-    expected_result = obj_dict
-    path = open('gendiff/tests/fixtures/test_make_str_if.txt')
-    expected_result_dumps = path.read()
+    path_2 = open('gendiff/tests/fixtures/test_make_str_else.json')
+    expected_result_dumps = path_2.read()
 
-    if isinstance(obj_dict, str):
-        assert make_str(obj_dict) == expected_result
+    if isinstance(expected_result, str):
+        assert make_str(expected_result) == expected_result
     else:
-        obj_json_dumps = json.dumps(obj_dict)
-        assert make_str(obj_json_dumps) == expected_result_dumps
+        assert make_str(expected_result) == expected_result_dumps
 
 
 def test_format_plain():
-    obj_dict = {
-        'common': {
-            'type': 'nested',
-            'value': None,
-            'children': {
-                'follow': {
-                    'type': 'added',
-                    'value': False,
-                    'children': None
-                },
-                'setting1': {
-                    'type': 'unchanged',
-                    'value': 'Value 1',
-                    'children': None
-                },
-                'setting2': {
-                    'type': 'removed',
-                    'value': 200,
-                    'children': None
-                },
-                'setting3': {
-                    'type': 'changed',
-                    'value': {
-                        'old_value': True,
-                        'new_value': None
-                    },
-                    'children': None
-                },
-                'setting4': {
-                    'type': 'added',
-                    'value': 'blah blah',
-                    'children': None
-                },
-                'setting5': {
-                    'type': 'added',
-                    'value': {
-                        'key5': 'value5'
-                    },
-                    'children': None
-                },
-                'setting6': {
-                    'type': 'nested',
-                    'value': None,
-                    'children': {
-                        'doge': {
-                            'type': 'nested',
-                            'value': None,
-                            'children': {
-                                'wow': {
-                                    'type': 'changed',
-                                    'value': {
-                                        'old_value': '',
-                                        'new_value': 'so much'
-                                    },
-                                    'children': None
-                                }
-                            }
-                        },
-                        'key': {
-                            'type': 'unchanged',
-                            'value': 'value',
-                            'children': None
-                        },
-                        'ops': {
-                            'type': 'added',
-                            'value': 'vops',
-                            'children': None
-                        }
-                    }
-                }
-            }
-        },
-        'group1': {
-            'type': 'nested',
-            'value': None,
-            'children': {
-                'baz': {
-                    'type': 'changed',
-                    'value': {
-                        'old_value': 'bas',
-                        'new_value': 'bars'
-                    },
-                    'children': None
-                },
-                'foo': {
-                    'type': 'unchanged',
-                    'value': 'bar',
-                    'children': None
-                },
-                'nest': {
-                    'type': 'changed',
-                    'value': {
-                        'old_value': {
-                            'key': 'value'
-                        },
-                        'new_value': 'str'
-                    },
-                    'children': None
-                }
-            }
-        },
-        'group2': {
-            'type': 'removed',
-            'value': {
-                'abc': 12345,
-                'deep': {
-                    'id': 45
-                }
-            },
-            'children': None
-        },
-        'group3': {
-            'type': 'added',
-            'value': {
-                'deep': {
-                    'id': {
-                        'number': 45
-                    }
-                },
-                'fee': 100500
-            },
-            'children': None
-        }
-    }
+    path_1 = open('gendiff/tests/fixtures/test_plain.json')
+    obj_dict = json.loads(path_1.read())
 
-    path = open('gendiff/tests/fixtures/test_result_plain.txt')
-    expected_result = path.read()
-
+    path_2 = open('gendiff/tests/fixtures/test_result_plain.txt')
+    expected_result = path_2.read()
     assert format_plain(obj_dict) == expected_result
