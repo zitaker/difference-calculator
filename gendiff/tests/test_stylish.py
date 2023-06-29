a58659d22b7b1a5e7af16ff0286f27a539f7f39d
georg <georbearwolf.com>
2023-06-29 09:17:50 +0300

    [073]

--- gendiff/tests/test_stylish.py
+++ gendiff/tests/test_stylish.py
@@ -1,80 +1,16 @@
-from gendiff.parser import file_parser
-from gendiff.create_diff_get import create_diff_get
 from gendiff.formaters.stylish import stringify
 from gendiff.formaters.stylish import format_stylish
-
-
-def test_create_diff_get():
-    path_1 = file_parser('gendiff/tests/fixtures/file1.json')
-    path_2 = file_parser('gendiff/tests/fixtures/file2.json')
-    actual_result = create_diff_get(path_1, path_2)
-
-    expected_result = {
-        "follow": {
-            "type": "removed",
-            "value": False,
-            "children": None
-        },
-        "host": {
-            "type": "unchanged",
-            "value": "hexlet.io",
-            "children": None
-        },
-        "proxy": {
-            "type": "removed",
-            "value": "123.234.53.22",
-            "children": None
-        },
-        "timeout": {
-            "type": "changed",
-            "value": {
-                "old_value": 50,
-                "new_value": 20
-            },
-            "children": None
-        },
-        "verbose": {
-            "type": "added",
-            "value": True,
-            "children": None
-        }
-    }
-    assert actual_result == expected_result
+import json
 
 
 def test_stringify():
-    obj_dict = {
-        'follow': {
-            'type': 'removed',
-            'value': False,
-            'children': None
-        },
-        'host': {
-            'type': 'unchanged',
-            'value': 'hexlet.io',
-            'children': None
-        },
-        'proxy': {
-            'type': 'removed',
-            'value': '123.234.53.22',
-            'children': None
-        },
-        'timeout': {
-            'type': 'changed',
-            'value': {
-                'old_value': 50,
-                'new_value': 20
-            },
-            'children': None
-        }, 'verbose': {
-            'type': 'added',
-            'value': True,
-            'children': None
-        }
-    }
+    path_1 = open('gendiff/tests/fixtures/test_stringify.json')
+    obj_dict = json.loads(path_1.read())
 
-    path = open('gendiff/tests/fixtures/test_result_stringify.txt')
-    expected_result = path.read()
+    path_2 = open('gendiff/tests/fixtures/test_result_stringify.txt')
+    expected_result = path_2.read()
+
+    print(type(expected_result))
     assert stringify(obj_dict, level=1) == expected_result
 
 
